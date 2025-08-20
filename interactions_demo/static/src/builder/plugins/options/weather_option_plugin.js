700628df6669056362dfeb89d463917c92527987
Robin Lejeune (role) <role@odoo.com>
2025-08-20 17:01:12 +0200

    Step 8.2: add builder options

--- interactions_demo/static/src/builder/plugins/options/weather_option_plugin.js
+++ interactions_demo/static/src/builder/plugins/options/weather_option_plugin.js
@@ -1,3 +1,4 @@
+import { BuilderAction } from "@html_builder/core/builder_action";
 import { layoutOptionSelector } from "@html_builder/utils/grid_layout_utils";
 import { Plugin } from "@html_editor/plugin";
 import { registry } from "@web/core/registry";
@@ -14,12 +15,42 @@ class WeatherOptionPlugin extends Plugin {
             {
                 template: "interactions_demo.WeatherOption",
                 selector: ".s_weather",
-                // /!\ Don't do that: this targets elements on the interaction,
-                // this won't work.
-                applyTo: ".card, .card-body",
+            },
+            {
+                template: "interactions_demo.WeatherThemeOption",
+                selector: ".s_weather",
+                applyTo: ".weather-cards-container",
             },
         ],
+        builder_actions: {
+            WeatherTitleAction,
+        },
     };
+}
+
+export class WeatherTitleAction extends BuilderAction {
+    static id = "weatherTitle";
+
+    isApplied({ editingElement, value }) {
+        return editingElement.querySelector("h2").classList.contains(value);
+    }
+    apply({ editingElement, value }) {
+        const titleEl = editingElement.querySelector("h2");
+        titleEl.classList.add(value);
+        const locationNameEl = titleEl.querySelector(".weather-location-name");
+
+        if (value === "classic") {
+            titleEl.textContent = "'s Weather";
+            titleEl.prepend(locationNameEl);
+        } else if (value === "unicorny") {
+            titleEl.textContent = "🦄🦄🦄🌈🌈 Weather in ";
+            titleEl.appendChild(locationNameEl);
+            titleEl.append(" ☀️☀️🦄🦄🦄");
+        }
+    }
+    clean({ editingElement, value }) {
+        editingElement.querySelector("h2").classList.remove(value);
+    }
 }
 
 registry.category("website-plugins").add(WeatherOptionPlugin.id, WeatherOptionPlugin);
